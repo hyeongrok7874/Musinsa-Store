957c152cfa1a6a0ef67048af05ce77d31a016cf7
hyeongrok7874 <hyeongrok7874@naver.com>
2021-08-30 16:15:09 +0900

    Add best products

--- script/function.js
+++ script/function.js
@@ -4,10 +4,11 @@ let leftMenu = document.getElementsByClassName("left-menu")[0];
 openButton.onclick = function () {
     if (leftMenu.style.display != "none") {
         leftMenu.style.display = "none";
-        openButton.style.right = "0px";
+        openButton.style.left = "0px";
     }
     else {
         leftMenu.style.display = "inline-block";
         openButton.style.right = "6px";
+        openButton.style.bottom = "383px";
     }
 }
